7589b8d6fd314db4b0499c78d82764a99ed68a62
pierluigigiancola <pierluigi.giancola@gmail.com>
2023-02-18 18:56:00 +0100

    feat: took CodeSnippet integration from astro doc repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
-
+import AutoImport from 'astro-auto-import';
+import { astroCodeSnippets, codeSnippetAutoImport } from './src/integrations/CodeSnippet'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pierluigigiancola.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		AutoImport({
+			imports: [
+				codeSnippetAutoImport,
+			],
+		}),
+		astroCodeSnippets(),
+		mdx(),
+		sitemap()],
 });
